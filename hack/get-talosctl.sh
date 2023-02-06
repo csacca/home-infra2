805c8ad86386b3a49124cd44da7ae6820e4a9bd1
Christopher Sacca <csacca@csacca.net>
2023-02-05 19:39:37 -0500

    move helpers

--- hack/get-talosctl.sh
+++ hack/get-talosctl.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+# renovate: datasource=github-releases depName=siderolabs/talos
 VERSION=v1.3.0
+
 curl -Lo /usr/local/bin/talosctl https://github.com/siderolabs/talos/releases/download/${VERSION}/talosctl-$(uname -s | tr "[:upper:]" "[:lower:]")-amd64
 chmod +x /usr/local/bin/talosctl
